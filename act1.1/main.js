a318228e9f77d6304587b493259ad568f16ea3b5
Daif <applediegoaif@gmail.com>
2025-08-31 19:41:24 -0600

    add(act1.1): Actividad 1.1

---
+++ act1.1/main.js
@@ -0,0 +1,6 @@
+import mergeSort from "./merge_sort.js";
+
+// Ejemplo de uso:
+const numeros = [38, 27, 43, 3, 9, 82, 10];
+console.log("Original:", numeros);
+console.log("Ordenado:", mergeSort(numeros));
