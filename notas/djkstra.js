1db4aa433d5328edace5702771c8f3fe357f9bbb
Daif <applediegoaif@gmail.com>
2025-10-27 18:41:49 -0600

    add(notas): Notas

---
+++ notas/djkstra.js
@@ -0,0 +1,9 @@
+import { input } from "./input";
+
+(async function main() {
+    const _nodes = await input('Nodos: ');
+    const nodes = nodes.split(' ');
+
+    const dist = Array(Infinity).fill(nodes.length);
+
+})();
